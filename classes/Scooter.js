43bf7d27e9246b808e310817a464d4c593454af9
Riley Everett <rileyeverett@Rileys-MacBook-Pro.local>
2024-10-25 12:11:12 +0100

    added constructors to classes

--- classes/Scooter.js
+++ classes/Scooter.js
@@ -1,5 +1,15 @@
 class Scooter {
+  static id =0;
   // Scooter code here
+  constructor(station,user,scooterId,charge,isBroken){
+    this.station =station;
+    this.user =user;
+    this.scooterId=scooterId;//give scootrer id the id number +=1
+    this.charge =charge;
+    this.isBroken =isBroken;
+
+
+  }
 }
 
 module.exports = Scooter;
--- classes/User.js
+++ classes/User.js
@@ -1,5 +1,12 @@
 class User {
   // User code here
+  constructor(username,password,age,loggedIn){
+    this.username=username;
+    this.password=password;
+    this.age=age;
+    this.loggedIn =loggedIn;
+    
+  }
 }
 
 module.exports = User;
